4a7f2c79564a9167d83888cbe028a533f5cc2836
Özgün Aksoy <64470656+Ozgunak@users.noreply.github.com>
2021-12-15 14:24:24 +0300

    first commit

--- GitHubHomework/ViewController.swift
+++ GitHubHomework/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var heightLabel: UITextField!
+    @IBOutlet weak var weightLabel: UITextField!
+    @IBOutlet weak var heightLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+    }
+    
 }
 
