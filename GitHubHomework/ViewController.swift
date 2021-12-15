a510ea15f93b71439c7b7be48a429960a91684dd
Özgün Aksoy <64470656+Ozgunak@users.noreply.github.com>
2021-12-15 17:02:15 +0300

    added slider and added calculation

--- GitHubHomework/ViewController.swift
+++ GitHubHomework/ViewController.swift
@@ -9,17 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var bmiLabel: UILabel!
-    @IBOutlet weak var heightLabel: UITextField!
-    @IBOutlet weak var weightLabel: UITextField!
+    
+    @IBOutlet weak var weightSlider: UISlider!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightLabel: UILabel!
     @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var bmiLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func WSlider(_ sender: UISlider) {
+        let weight = String(format: "%.0f", sender.value)
+        weightLabel.text = "\(weight)Kg"
+    }
+    @IBAction func HSlider(_ sender: UISlider) {
+        let height = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(height)m"
+    }
     @IBAction func buttonTapped(_ sender: UIButton) {
-        
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        let BMI = weight / (height * height)
+        print(BMI)
+        bmiLabel.text = String(format: "%.1f", BMI)
     }
     
 }
